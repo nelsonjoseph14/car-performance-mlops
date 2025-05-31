031447bdfe1918ab51bd63d125f6742c8645fd24
Nelson Jason Joseph <141666026+nelsonjoseph14@users.noreply.github.com>
2025-05-31 21:09:36 +0000

    added another unit test

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,3 +5,8 @@ def test_model_predicts_valid_class():
     sample = pd.DataFrame([[2018, "BMW 3"]], columns=["Year", "Model"])
     pred = model.predict(sample)
     assert pred[0] in ["Good", "Bad"]
+
+def test_model_has_predict():
+    import joblib
+    model = joblib.load("model/model.pkl")
+    assert hasattr(model, "predict")
